747e0f40ac2b00405cf59f62ff2b940f3922f511
fakihfavian24 <fakihfavianwibowo@gmail.com>
2023-12-06 18:38:28 +0700

    Add Form Comment

--- src/scripts/views/pages/detail-post.js
+++ src/scripts/views/pages/detail-post.js
@@ -6,19 +6,18 @@ import detailPostCreator from '../templates/detail-post-creator';
 const DetailPost = {
   async render() {
     return `
-      <jumbotron-section></jumbotron-section>
       <section class="detail">
       <div class="container" id="detailMotor"></div>
       </section>
       `;
-    },
+  },
 
   async afterRender() {
-      const url = UrlParser.parseActiveUrlWithoutCombiner();
-      const motor = await MotorSource.detailMotor(url.id);
-      console.log(motor);
-      const detailContainer = document.querySelector('#detailMotor');
-      detailContainer.innerHTML = detailPostCreator(motor);
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const motor = await MotorSource.detailMotor(url.id);
+    console.log(motor);
+    const detailContainer = document.querySelector('#detailMotor');
+    detailContainer.innerHTML = detailPostCreator(motor);
   },
 };
 
--- src/scripts/views/templates/detail-post-creator.js
+++ src/scripts/views/templates/detail-post-creator.js
@@ -26,13 +26,38 @@ const detailPostCreator = (motor) => `
             <p class="description">${motor.description}</p>
 
             <div class="icon-detail">
-            <a class="icon-comment" href="#/"><i class="fa-regular fa-comment"></i></a>
+
+            <button type="button" class="btn btn-comment" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <i class="fa-regular fa-comment"></i>
+            </button>
+
+            
             <a class="icon-edit" href="#/edit"><i class="fa-regular fa-pen-to-square"></i></a>
             </div>
 
           </div>
 
           <div class="comment">
+
+          <h2>Comments</h2>
+
+          <!-- Modal -->
+          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <p class="user-comment"><i class="fa-solid fa-user"></i> John Doe</p>
+                </div>
+                <div class="modal-body">
+                <input class="form-control form-control-sm" type="text" placeholder="add comment" aria-label=".form-control-sm example">
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-submit-comment" data-bs-dismiss="modal"><i class="fa-solid fa-paper-plane"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           ${motor.comments.map((review) => `
             <p class="user-comment"><i class="fa-solid fa-user"></i> ${review.author.name}</p>
             <p>${review.body}</p>
