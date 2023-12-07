5e5dcb6875c85cfd065a14e5da4fce8b70a1a7c8
fakihfavian24 <fakihfavianwibowo@gmail.com>
2023-12-07 19:21:38 +0700

    Fitur Posting

--- src/scripts/data/motor-source.js
+++ src/scripts/data/motor-source.js
@@ -1,33 +1,33 @@
 import API_ENDPOINT from "../globals/api-endpoint";
 
 class MotorSource {
-    static async listMotor() {
-      try {
-        const response = await fetch(API_ENDPOINT.LIST);
-        if (!response.ok) {
-          throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-        }
-        const responseJson = await response.json();
-        return responseJson.motors;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
+  static async listMotor() {
+    try {
+      const response = await fetch(API_ENDPOINT.LIST);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
       }
-    }
-  
-    static async detailMotor(id) {
-      try {
-        const response = await fetch(API_ENDPOINT.DETAIL(id));
-        if (!response.ok) {
-          throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-        }
-        const responseJson = await response.json();
-        return responseJson.motor;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-      }
+      const responseJson = await response.json();
+      return responseJson.motors;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
     }
   }
-  
-  export default MotorSource;
+
+  static async detailMotor(id) {
+    try {
+      const response = await fetch(API_ENDPOINT.DETAIL(id));
+      if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+      }
+      const responseJson = await response.json();
+      return responseJson.motor;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  }
+}
+
+export default MotorSource;
--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,8 +1,8 @@
 import CONFIG from "./config";
 
 const API_ENDPOINT = {
-    LIST: `${CONFIG.BASE_URL_API}motors`,
-    DETAIL: (id) => `${CONFIG.BASE_URL_API}motors/detail/${id}`,
-  };
-  
+  LIST: `${CONFIG.BASE_URL_API}motors`,
+  DETAIL: (id) => `${CONFIG.BASE_URL_API}motors/detail/${id}`,
+};
+
 export default API_ENDPOINT;
