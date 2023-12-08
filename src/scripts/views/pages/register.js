5f4f4d5671ae447c5f4cf4ff4284b56a64104b91
Dennis Havinanda <dennis10.havinanda@gmail.com>
2023-12-08 22:33:42 +0700

    try regist and post

--- src/scripts/views/pages/register.js
+++ src/scripts/views/pages/register.js
@@ -1,3 +1,5 @@
+import MotorSource from "../../data/motor-source";
+
 const Register = {
   async render() {
     return `
@@ -9,19 +11,25 @@ const Register = {
 
             <h2>Register Form</h2>
             <p>Please register here</p>
+            <form id="formRegister">
+              <div class="form-register mb-3">
+                <label for="username" class="form-label">username</label>
+                <input type="username" class="form-control" id="username" placeholder="Enter your email">
+                
+                <label for="name" class="form-label">name</label>
+                <input type="name" class="form-control" id="name" placeholder="Enter your email">
+                
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter your email">
 
-            <div class="form-register mb-3">
-              <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+              </div>
 
-              <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password" placeholder="Enter your password">
-            </div>
-
-            <div class="d-grid gap-2">
-              <button class="btn-register btn" type="button">Register</button>
-            </div>
-            
+              <div class="d-grid gap-2">
+                <button class="btn-register btn" type="submit">Register</button>
+              </div>
+            </form>
             <div class="already-have-account">
             <p>Already Have Account? <a href="#/login">Click Here</a>
             </p></div>
@@ -40,7 +48,32 @@ const Register = {
   },
 
   async afterRender() {
+    const form = document.getElementById('formRegister');
 
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const username = document.getElementById('username').value;
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      if(!username || !name || !email || !password){
+        alert('Isi woy');
+        return;
+      }
+
+      const dataInput = {
+        username,
+        name,
+        email,
+        password,
+      };
+
+      await MotorSource.register(dataInput);
+      
+    });
+    form.reset();
   },
 };
 
