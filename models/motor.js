ad3d5433cf901084828bc191933a0f88c8f53044
Kinaryo <kinaryo733huda@gmail.com>
2023-12-17 20:42:52 +0700

    fig bug tanggal

--- models/motor.js
+++ models/motor.js
@@ -1,39 +1,38 @@
-const mongoose = require('mongoose')
-const Comment = require("./comment")
+const mongoose = require('mongoose');
+const Comment = require('./comment');
 const Schema = mongoose.Schema;
 
-const motorSchema = new Schema ({
-    title: String,
-    licensePlate: String,
-    model:String,
-    description: String,
-    dateTime: {
-      type: String, // Menggunakan tipe String
-      default: () => {
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-        const day = currentDate.getDate().toString().padStart(2, '0');
-        return `${year}-${month}-${day}`;
-      },
-      },
-      imageURL: [{
-        type: String}],
-
-    author : {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
+const motorSchema = new Schema({
+  title: String,
+  licensePlate: String,
+  model: String,
+  description: String,
+  dateTime: {
+    type: Date,
+    default: Date.now,
   },
-    comments:[{
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }]
+  imageURL: [{
+    type: String
+  }],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+});
 
-})
+motorSchema.methods.getShortFormattedDate = function () {
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  return this.dateTime.toLocaleDateString('en-US', options);
+};
 
-motorSchema.post(`findOneAndDelete`, async function(doc) {
-  if(doc){
-    await Comment.deleteMany({_id:{$in:doc.comments}})
+motorSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Comment.deleteMany({ _id: { $in: doc.comments } });
   }
-})
-module.exports = mongoose.model ('Motor',motorSchema)
+});
+
+module.exports = mongoose.model('Motor', motorSchema);
