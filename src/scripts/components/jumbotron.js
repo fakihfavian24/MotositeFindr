93dc6ccdf05ba2c81eca59832c75c716be679b39
Dennis Havinanda <dennis10.havinanda@gmail.com>
2023-12-18 14:35:23 +0700

    update jumbroton and add meta

--- src/scripts/components/jumbotron.js
+++ src/scripts/components/jumbotron.js
@@ -12,39 +12,24 @@ class Jumbotron extends HTMLElement {
     return this.querySelector("#searchInput").value;
   }
 
-  handleSearch() {
-    const searchInputValue = this.value;
-
-    // Dispatch a custom event with the search input value
-    const searchEvent = new CustomEvent('search', {
-      detail: { searchInputValue },
-    });
-    this.dispatchEvent(searchEvent);
-  }
-
   render() {
     this.innerHTML = `
-      <section class="jumbotron">
-        <div class="p-5 mb-1">
-          <div class="container-fluid py-5">
-            <h1 class="fw-bold text-center pt-5" data-aos="fade-right">Find Your Lost Motorcycle</h1>
-            <p class="text-center" data-aos="fade-left">Enter the details of your lost motorcycle to start the search</p>
-            <div>
-              <div class="form-input">
-                <input id="searchInput" class="form-control" type="text" placeholder="Enter motor model" aria-label="default input example">
-              </div>
-              <div class="text-center mb-1">
-                <!-- Use a click event listener to trigger the handleSearch function -->
-                <button class="btn-search" type="button" id="searchButton">Search</button>
-              </div>
+    <section class="jumbotron">
+      <div class="p-5 mb-1">
+        <div class="container-fluid py-5">
+          <h1 class="fw-bold text-center pt-5" data-aos="fade-right">Find Your Lost Motorcycle</h1>
+          <p class="text-center" data-aos="fade-left">Come find your motorbike easily and for free</p>
+          <div
+            <div class="text-center mb-1">
+              <a href="#/searchpages"><button class="btn-search" type="button" id="searchButton">Get Started</button></a>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
     `;
 
-    // Attach the handleSearch function to the click event of the search button
-    this.querySelector('#searchButton').addEventListener('click', () => this.handleSearch());
+    this.querySelector('#searchButton').addEventListener('click', this._clickEvent);
   }
 }
 
